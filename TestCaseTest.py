8949257a3691132cbfff977d4ee97fc873bcfc46
이동호(Nick) <intoday87@coupang.com>
2018-03-04 22:16:46 +0900

    먼저 setUp 호출하기

--- TestCaseTest.py
+++ TestCaseTest.py
@@ -1,26 +1,39 @@
 class TestCase(object):
     def __init__(self, name):
-        self.name = name;
+        self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+
+    def setUp(self):
+        self.wasSetup = 1
 
 
 class WasRun(TestCase):
     def __init__(self, name):
+       TestCase.__init__(self, name)
+
+    def setUp(self):
         self.wasRun = None
-        TestCase.__init__(self, name)
+        self.wasSetup = 1
 
     def testMethod(self):
         self.wasRun = 1
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun('testMethod')
+
     def testRunning(self):
-        test = WasRun('testMethod')
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetup(self):
+        self.test.run()
+        assert self.test.wasSetup
 
 TestCaseTest('testRunning').run()
+TestCaseTest('testSetup').run()
